15f95391a5e82f36465e68d93b2646add5ef62ac
Manuel <m.frick@x12.ch>
2022-01-15 10:00:44 +0100

    Update Header.tsx

--- website/src/shared/Header.tsx
+++ website/src/shared/Header.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
       <Toolbar>
         <Link href={home} color="inherit" underline="none">
           <Typography variant="h6" component="div">
-            Yopass
+            Zettapass
             <Box
               sx={{
                 verticalAlign: 'middle',
